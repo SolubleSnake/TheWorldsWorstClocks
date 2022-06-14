43975404c1b14fdc86793de94bcedc66d374a78e
User <alex.jermy@hotmail.co.uk>
2022-06-14 14:29:04 +0100

    Colours now fully working. Maybe need to change later. Minutes and seconds multiplied by 4. Also loads once. Need to make it dynamic.

--- The World's Worst Clocks/Pages/Index.cshtml.cs
+++ The World's Worst Clocks/Pages/Index.cshtml.cs
@@ -19,9 +19,29 @@ namespace The_World_s_Worst_Clocks.Pages
 
         public string TimeColour { get; set; } = "PageModel in C#";
 
+        public string RGBHours { get; set; } = DateTime.Now.TimeOfDay.Hours.ToString();
+        public int RGBRed { get; set; } = 1;
+        public string RGBMinutes { get; set; } = DateTime.Now.TimeOfDay.Minutes.ToString();
+        public int RGBGreen { get; set; } = 1;
+        public String RGBSeconds { get; set; } = DateTime.Now.TimeOfDay.Seconds.ToString();
+        public int RGBBlue { get; set; } = 1;
+
         public void OnGet()
         {
             TimeColour += $"Server time is {DateTime.Now}";
+
+            RGBRed = Int32.Parse(RGBHours) * 10;
+
+            RGBRed = RGBRed;
+
+            RGBGreen = Int32.Parse(RGBMinutes) * 4;
+
+            RGBGreen = RGBGreen;
+
+            RGBBlue = RGBBlue = Int32.Parse(RGBSeconds) * 4;
+
+            RGBBlue = RGBBlue;
+
         }
 
 
